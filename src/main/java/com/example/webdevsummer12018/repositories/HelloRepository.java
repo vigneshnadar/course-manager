64cd19e6a346051aa11e52ccc7a96f9e496cf21a
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-05-13 16:05:05 -0400

    added hello model, repository and server

---
+++ src/main/java/com/example/webdevsummer12018/repositories/HelloRepository.java
@@ -0,0 +1,10 @@
+package com.example.webdevsummer12018.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.webdevsummer12018.models.Hello;
+
+
+public interface HelloRepository
+extends CrudRepository<Hello, Integer> {}
+
+
