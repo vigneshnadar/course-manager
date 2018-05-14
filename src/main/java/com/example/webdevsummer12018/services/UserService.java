631d2bb0df29f58fad817b7c85bc53f2864cd59e
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-05-13 23:27:01 -0400

    user details post mapping

--- src/main/java/com/example/webdevsummer12018/services/UserService.java
+++ src/main/java/com/example/webdevsummer12018/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webdevsummer12018.models.User;
@@ -18,6 +20,12 @@ public class UserService {
 	@GetMapping("/api/user")
 	public List<User> findAllUsers() {
 		return (List<User>) repository.findAll();
+	}
+	
+	
+	@PostMapping("/api/user")
+	public User createUser(@RequestBody User user) {
+		return repository.save(user);
 	}
 
 }
--- src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -14,12 +14,39 @@
         tbody = $('tbody');
         template = $('.template');
 
+        $('#createUser').click(createUser);
+
         var promise = fetch('http://localhost:8080/api/user');
 
         promise.then(function (response) {
             return response.json();
         }).then(renderUsers);
 
+    }
+
+
+    function createUser() {
+        console.log('xyz')
+
+        var username = $('#usernameField').val();
+        var password = $('#passwordField').val();
+        var firstName = $('#firstnameField').val();
+        var lastName = $('#lastnameField').val();
+
+        var user = {
+             username : username,
+             password : password,
+            firstName : firstName,
+            lastName : lastName
+        };
+
+        fetch('http://localhost:8080/api/user', {
+            method : 'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
     }
 
     function renderUsers(users) {
